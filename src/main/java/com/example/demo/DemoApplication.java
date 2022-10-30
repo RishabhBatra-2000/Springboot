a5fb8c77d00df5cf8f43d0011d1b220d91078d83
RishabhBatra-2000 <batrar68@gmail.com>
2022-10-30 19:57:44 +0530

    Initial version

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,9 +18,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping
-	public List<String> hello() {
+	public List<Student> hello() {
 		// returns a hello world in json format
-		return List.of("Hello","world");
+		return List.of(new Student(1L,"rishabh batra","batrar68@gmail.com",LocalDate.of(2000, Month.APRIL,20),22));
 	}
 
 }
